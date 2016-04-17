ec091a0152c3387ddd155e15a919e85316158a1f
Jun Zheng <arthurjordan@gmail.com>
2016-04-17 10:12:45 +1000

    first commit for react-simple-components

--- app/main.js
+++ app/main.js
@@ -2,18 +2,3 @@ import '../public/css/style.css';
 
 import ReactDOM from 'react-dom';
 import React from 'react';
-
-let CommentBox = React.createClass({
-  render: function(){
-    return (
-      <div className='commentBox'>
-        <h1>Juns CommentBox 1213</h1>
-      </div>
-    )
-  },
-})
-
-ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('content')
-);
